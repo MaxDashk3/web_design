446c376d3f536fc4088c11bbbd41917a47271307
Maxim Dashkovskiy <maxdashk3@gmail.com>
2023-10-23 18:04:14 +0300

    Added lab10

---
+++ Lz10/Lz10/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lz10.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
